5f607492f54fca13dfd97cda595dfe4a03ecd309
kaikillgerda <108028047+kaikillgerda@users.noreply.github.com>
2022-11-17 18:59:25 +0300

    Шестое домашнее зажание по С#

---
+++ 6_homework/Task_01/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+int[] CreateArray(string userInput)
+{
+    int counter = 1;
+    for (int i = 0; i < userInput.Length; i++)
+    {
+        if (userInput[i] == ' ')
+        {
+            counter++;
+        }
+    }
+
+    int[] numbers = new int[counter];
+    int index = 0;
+
+    for (int i = 0; i < userInput.Length; i++)
+    {
+        string temp = "";
+
+        while (userInput[i] != ' ')
+        {
+            if (i != userInput.Length - 1)
+            {
+                temp += userInput[i].ToString();
+                i++;
+            }
+            else
+            {
+                temp += userInput[i].ToString();
+                break;
+            }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Массив из введенных значений: [" + $"{array[0]}");
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int FindOverZeroNumbers(int[] numbers)
+{
+    int counter = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0) counter++;
+    }
+    return counter;
+}
+
+void Runner()
+{
+    Console.Write("Пожалуйста, введите несколько чисел: ");
+    int[] array = CreateArray(Console.ReadLine());
+    PrintArray(array);
+    Console.WriteLine($"Количество чисел больше 0 = {FindOverZeroNumbers(array)}");
+}
+
+Runner();
---
+++ 6_homework/Task_03/Program.cs
@@ -0,0 +1,99 @@
+﻿/* Задача 3. (*) Найдите максимальное значение в матрице по каждой строке, получите сумму этих максимумов. 
+Затем найдите минимальное значение по каждой колонке,получите сумму этих минимумов.
+Затем из первой суммы (с максимумами) вычтите вторую сумму(с минимумами)
+1 2 3
+3 4 5
+3+5=8, 1+2+3=6, 8-6=2 */
+int Prompt(string message)
+{
+    Console.Write(message + " -> ");
+    return int.Parse(Console.ReadLine());
+}
+
+void CreateMatrix(int[,] matr)
+{
+    Console.WriteLine("Матрица с заданным количеством строк и столбцов создана: ");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix(int[,] matr)
+{
+    int min = Prompt("Пожалуйста, введите минимальное значение элементов матрицы");
+    int max = Prompt("Пожалуйста, введите максимальное значение элементов матрицы");
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max);
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int LineMax(int[,] matr)
+{
+    Console.Write("Максимальные значения элементов в каждой строке: ");
+
+    int sumLineMax = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        int lineMax = matr[i, 0];
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i, j] > lineMax)
+                lineMax = matr[i, j];
+        }
+        sumLineMax += lineMax;
+        Console.Write($"{lineMax} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма максимальных элементов строк: {sumLineMax}");
+    return sumLineMax;
+}
+
+int ColumnMin(int[,] matr)
+{
+    Console.Write("Минимальное значение элемента в каждом стролбце: ");
+
+    int sumColumnMin = 0;
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        int columnMin = matr[0, j];
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            if (matr[i, j] < columnMin)
+                columnMin = matr[i, j];
+        }
+        sumColumnMin += columnMin;
+        Console.Write($"{columnMin} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма минимальных элементов столбцов: {sumColumnMin}");
+    return sumColumnMin;
+}
+
+void Runner()
+{
+    int lineLen = Prompt("Пожалуйста, введите количество строк матрицы");
+    int columnLen = Prompt("Пожалуйста, введите количество столбцов матрицы");
+    int[,] matrix = new int[lineLen, columnLen];
+    CreateMatrix(matrix);
+    Console.WriteLine();
+    FillMatrix(matrix);
+    int max = LineMax(matrix);
+    Console.WriteLine();
+    int min = ColumnMin(matrix);
+    Console.WriteLine();
+    int result = max - min;
+    Console.WriteLine($"Разность сумм максимальных значений строк и минимальных значений столбцов равна: {result}");
+}
+
+Runner();

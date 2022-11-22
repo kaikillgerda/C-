426fa638d490faa3fc5cbe3354e8b4d84ad64efd
kaikillgerda <108028047+kaikillgerda@users.noreply.github.com>
2022-11-22 13:34:06 +0300

    Восьмой семинар по С#

---
+++ 8_seminar/Task_00_Prompt/Program.cs
@@ -0,0 +1,27 @@
+﻿// Проверка вводимых пользователем данных
+
+
+int PromptInt(string strMessage)
+{
+    Console.Write(strMessage);
+    int temp;
+    if (int.TryParse(Console.ReadLine(), out temp))
+    {
+        return temp;
+    }
+
+    throw new Exception("Это не число!!!");
+}
+
+int Prompt(string message)
+{
+    while (true)
+    try
+    {
+        return PromptInt(message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Вы чтото неправильно ввели. Ошибка: {e.Message}");
+    }
+}
--- 8_seminar/Task_01/Program.cs
+++ 8_seminar/Task_01/Program.cs
@@ -1,24 +1,68 @@
-﻿int PromptInt(string strMessage)
-{
-    Console.Write(strMessage);
-    int temp;
-    if (int.TryParse(Console.ReadLine(), out temp))
-    {
-        return temp;
-    }
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
 
-    throw new Exception("Это не число!!!");
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int userInput = Convert.ToInt32(Console.ReadLine());
+    return userInput;
 }
 
-int Prompt(string message)
+int[,] CreateArray(int rows, int columns, int lowerLim, int upperLim)
 {
-    while (true)
-    try
+    int[,] table = new int[rows, columns];
+    for (int i = 0; i < table.GetLength(0); i++)
     {
-        return PromptInt(message);
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().Next(lowerLim, upperLim);
+        }
     }
-    catch (Exception e)
-    {
-        Console.WriteLine($"Вы чтото неправильно ввели. Ошибка: {e.Message}");
-    }
+    return table;
 }
+int[,] ChangeFirstLastRows(int[,] baseArray)
+{
+    const int i = 0;
+    int k = baseArray.GetLength(0) - 1;
+    for (int j = 0; j < baseArray.GetLength(1); j++)
+    {
+        Swap(baseArray, i, j, k, j);
+    
+    }
+    return baseArray;
+
+}
+
+void Swap(int[,] baseArray, int i1, int j1, int i2, int j2)
+{
+    // int temp = baseArray[i1,j1];
+    // baseArray[i1,j1] = baseArray[i2, j2];
+    // baseArray[i2, j2] = temp;
+    (baseArray[i1,j1], baseArray[i2, j2])=(baseArray[i2,j2], baseArray[i1,j1]);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void Runner()
+{
+    int rows = Prompt("Enter quantity of raws ");
+    int columns = Prompt("Enter quantity of columns ");
+    int lowLim = Prompt("Enter lower limit for array element value");
+    int upperLim = Prompt("Enter upper limit for array element value");
+    int [,] baseArray = CreateArray(rows, columns, lowLim, upperLim);
+    PrintArray(baseArray);
+    Console.WriteLine();
+    int [,] changedArray = ChangeFirstLastRows(baseArray);
+    PrintArray(changedArray);
+}
+Runner();

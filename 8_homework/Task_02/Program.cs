46dae84a94096af3d8b78d1dbc39b81dfc48d2e0
kaikillgerda <108028047+kaikillgerda@users.noreply.github.com>
2022-11-23 23:18:49 +0300

    восьмая домашняя работа по С#

--- 8_homework/Task_02/Program.cs
+++ 8_homework/Task_02/Program.cs
@@ -1,13 +1,17 @@
-﻿/* Задача 1: Задайте двумерный массив. Напишите программу, 
-которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿/* Задача 2: Задайте прямоугольный двумерный массив.
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 Например, задан массив:
+
 1 4 7 2
+
 5 9 2 3
+
 8 4 2 4
-В итоге получается вот такой массив:
-7 4 2 1
-9 5 3 2
-8 4 4 2 */
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке 
+и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
 
 int Prompt(string message)
 {
@@ -41,24 +45,27 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-void ArrangeRowsNumbers(int[,] matrix)
+void LineSum(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
-        for (int j = 0; j < matrix.GetLength(1); j++)
-            for (int k = 0; k < matrix.GetLength(1); k++)
-            {
-                if (matrix[i, j] > matrix[i, k])
-                {
-                    int temp = matrix[i, j];
-                    matrix[i, j] = matrix[i, k];
-                    matrix[i, k] = temp;
-                }
-            }
-    for (int i = 0; i < matrix.GetLength(0); i++, Console.WriteLine())
+    int lineSum1 = 0, row = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        lineSum1 += matrix[0, i];
+    }
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        int lineSum2 = 0;
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i, j]}\t");
+            lineSum2 += matrix[i, j];
         }
+        if (lineSum1 > lineSum2)
+        {
+            lineSum1 = lineSum2;
+            row = i;
+        }
+    }
+    Console.WriteLine($"Наименьшая сумма элементов составляет: {lineSum1}\nНаходится в строке: {row + 1} ");
 }
 
 void Main()
@@ -70,6 +77,6 @@ void Main()
     int[,] matrix = CreateMatrix(rowsLength, columnLength, minValue, maxValue);
     PrintMatrix(matrix);
     Console.WriteLine();
-    ArrangeRowsNumbers(matrix);
+    LineSum(matrix);
 }
 Main();
---
+++ 8_homework/Task_03/Program.cs
@@ -0,0 +1,91 @@
+﻿/* Задача 3: Задайте две матрицы.
+Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+int Prompt(string message)
+{
+    Console.Write(message + " -> ");
+    int userInput = Convert.ToInt32(Console.ReadLine());
+    return userInput;
+}
+
+int[,] CreateMatrix(int rowsLength, int columnLength, int minValue, int maxValue)
+{
+    int[,] tempMatrix = new int[rowsLength, columnLength];
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return tempMatrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool ValidateMatrix(int[,] matrix1, int[,] matrix2)
+{
+    if (matrix1.GetLength(1) != matrix2.GetLength(0))
+    {
+        Console.WriteLine("Умножение таких матриц невозможно!");
+        return false;
+    }
+    return true;
+}
+
+void MultiplicationMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] matrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                matrix[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Main()
+{
+    int rowsLength1 = Prompt("Пожалуйста, введите количество строк первой матрицы");
+    int columnLength1 = Prompt("Пожалуйста, введите количество столбцов первой матрицы");
+    int minValue1 = Prompt("Пожалуйста, введите минимальное значение элементов первой матрицы");
+    int maxValue1 = Prompt("Пожалуйста, введите максимальное значение элементов первой матрицы");
+    int[,] matrix1 = CreateMatrix(rowsLength1, columnLength1, minValue1, maxValue1);
+    int rowsLength2 = Prompt("Пожалуйста, введите количество строк второй матрицы");
+    int columnLength2 = Prompt("Пожалуйста, введите количество столбцов второй матрицы");
+    int minValue2 = Prompt("Пожалуйста, введите минимальное значение элементов второй матрицы");
+    int maxValue2 = Prompt("Пожалуйста, введите максимальное значение элементов второй матрицы");
+    int[,] matrix2 = CreateMatrix(rowsLength2, columnLength2, minValue2, maxValue2);
+    Console.WriteLine("Первая матрица: ");
+    PrintMatrix(matrix1);
+    Console.WriteLine("Вторая матрица: ");
+    PrintMatrix(matrix2);
+    Console.WriteLine("Результат умножения матриц");
+    if (ValidateMatrix(matrix1, matrix2))
+    {
+        MultiplicationMatrix(matrix1, matrix2);
+    }
+}
+Main();
